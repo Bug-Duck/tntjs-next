e39b99b1ff6ecd03cc4b82ba2a0e0b4fb48a8b83
Sam Zhang <samzhangjy@outlook.com>
2022-07-16 17:13:32 +0800

    feat: added TNTApp hooks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { deepClone } from "./lib/common";
-import { watchEffect } from "./reactivity";
+import { computed, reactive, watchEffect } from "./reactivity";
 import {
   createVdomFromExistingElement,
   getAttributesOfElement,
@@ -9,16 +9,43 @@ import {
   VNode,
 } from "./vdom";
 
+export type TNTData = object;
+
+export type TNTEffect = () => any;
+
+export type TNTComputed = Record<string, TNTEffect>;
+
+// tweak the Window object a bit to pass type check
+declare global {
+  export interface Window {
+    data: TNTData;
+  }
+}
+
 /** Create a new TNTjs application. */
 export class TNTApp {
+  /** Reactive data object */
+  data: object;
+  #onMounted: (app: TNTApp) => void;
+  #hooksCalled: string[];
+
+  constructor() {
+    this.#onMounted = () => {};
+    this.#hooksCalled = [];
+    this.data = {};
+    window.data = {};
+  }
+
   /**
    * Initialize and mount a new TNT Application.
    * @param container The container element to mount with.
+   * @returns Mounted TNTApp instance.
    */
   mount(container: Element) {
     let isMounted = false;
     let prevVdom: VNode | null = null;
     let currentNode = null;
+    this.#hooksCalled.push("mount");
 
     // app lifecycle loop
     watchEffect(() => {
@@ -41,6 +68,7 @@ export class TNTApp {
         mount(prevVdom, container);
         isMounted = true;
         this.#removeUpdatedElements(container, currentContainer);
+        this.#onMounted(this);
         return;
       }
       const newVdom: VNode = deepClone(currentNode);
@@ -48,6 +76,61 @@ export class TNTApp {
       prevVdom = newVdom;
       this.#removeUpdatedElements(container, currentContainer);
     });
+
+    return this;
+  }
+
+  /**
+   * Hook to create reactive data objects.
+   * @param data Data to become reactive.
+   * @returns Current `TNTApp` instance.
+   */
+  useData(data: TNTData) {
+    this.#hooksCalled.push("data");
+    this.data = { ...this.data, ...reactive(data) };
+    window.data = this.data;
+    return this;
+  }
+
+  /**
+   * Hook to create computed values with ease.
+   * @param computedValues Functions to calcuate each computed value.
+   * @returns Current `TNTApp` instance.
+   */
+  useComputed(computedValues: TNTComputed) {
+    this.#hooksCalled.push("computed");
+    if (!this.#hooksCalled.includes("data")) {
+      console.warn(
+        "[TNT warn] useComputed() hook is called before useData(). Any reactive data accessed from computed functions will not be accessable.",
+        "This may lead to unpredictable results or errors."
+      );
+    }
+    for (const key in computedValues) {
+      this.data[key] = computed(computedValues[key]);
+    }
+    window.data = this.data;
+    return this;
+  }
+
+  /**
+   * Hook to watch effect dependency updates.
+   * @param effect Effect function to watch.
+   * @returns Current `TNTApp` instance.
+   */
+  useEffect(effect: TNTEffect) {
+    this.#hooksCalled.push("effect");
+    watchEffect(effect);
+    return this;
+  }
+
+  /**
+   * Run the specified effect when application is mounted.
+   * @param effect Effect to run when application is mounted.
+   * @returns Current `TNTApp` instance.
+   */
+  onMounted(effect: TNTEffect) {
+    this.#onMounted = effect;
+    return this;
   }
 
   /**
