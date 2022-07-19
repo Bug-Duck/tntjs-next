d3e0ee328750817ec7a092ad345feb9a5472adf0
sheepbox8646 <78635021+sheepbox8646@users.noreply.github.com>
2022-07-19 21:27:18 +0800

    Bug: <t-get> have a small bug.

--- src/renderers/getRenderer.ts
+++ src/renderers/getRenderer.ts
@@ -7,17 +7,28 @@ const getRenderer = (
   currentNode: VNode,
   extraContext: object
 ): RenderedContent => {
+  let buffer: {};
   // get url datas from src
   const url = currentNode.props.src;
   const type = currentNode.props.type;
   const http = new XMLHttpRequest();
-  if (type === "text" || type === undefiend) {
+  http.open("GET", url, true);
+  http.send();
+  const result = http.responseText;
+  if (type === "text" || type === undefined) {
+    currentNode.children = [
+      evaluate(result, extraContext).toString()
+    ]
+  } else if (type === "json") {
+    const datas = JSON.parse(result);
+    for (const data of datas) {
+      buffer[data] = reactive(evaluate(datas[data]));
+    };
   }
 
-  // const localVar = reactive(evaluate(currentNode.props.value, extraContext));
   return {
     shouldRender: true,
-    injectVariables: { [currentNode.props.key]: localVar },
+    injectVariables: buffer,
   };
 };
 
