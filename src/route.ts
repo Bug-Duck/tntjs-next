2e3b9f8d3350a201011ee20148cabef4a8e1b94d
sheepbox8646 <78635021+sheepbox8646@users.noreply.github.com>
2022-07-23 20:35:10 +0800

    Co-authored-by: Sam Zhang <samzhangjy@outlook.com> Co-authored-by: RULoN <jkh675@users.noreply.github.com>

--- src/route.ts
+++ src/route.ts
@@ -8,8 +8,10 @@ export class Route {
      * 2.当hash(#后面的值)发生变化,调用一次change
      * 3.change变化之后,改变url hash的值
      */
+    console.log("%c route init finish!", "color: red");
     this.#change(window.location.hash.replace(/#/, ""));
     window.onhashchange = () => {
+      console.log("%c hash change!", "color: red");
       this.#change(window.location.hash.replace(/#/, ""));
     };
     this.#routeList = [];
@@ -21,9 +23,10 @@ export class Route {
    * @param {r} obj path and element (route object)
    * @returns {any}
    */
-  useRoute(obj: r) {
+  useRoute(obj: r): void {
     this.#routeList.push(obj);
     this.#change(obj.path);
+    console.log("%c Use route finish!", "color: red");
   }
 
   /**
@@ -32,25 +35,29 @@ export class Route {
    * @param {String} path main page path
    * @returns {any}
    */
-  useMainRoute(path: String): any {
+  useMainRoute(path: String): void {
     this.#main = path;
+    console.log("%c Set mainly route!", "color: red");
   }
 
   toggle(path: String) {
-    window.history.pushState({}, document.title, path.toString());
+    this.#change(window.location.hash.replace(/#/, ""));
+    window.history.pushState({}, document.title, `#${path.toString()}`);
+    console.log("%c hash change!", "color: red");
+    // window.location.hash = path.toString();
   }
 
   #change(path: String) {
     /** */
-    if (typeof this.#main === "undefined") return;
+    if (typeof this.#main === "undefined") return console.log("%c Change route finish!", "color: red");
     if (path === "") {
       try {
         this.#routeList.forEach((i) => {
           if (i.path === this.#main) {
-            i.ele.style.visibility = "visible";
+            i.ele.style.display = "block";
             throw new Error("break");
           } else {
-            i.ele.style.visibility = "hidden";
+            i.ele.style.display = "none";
           }
         });
       } catch (e) {
@@ -59,13 +66,14 @@ export class Route {
     } else {
       this.#routeList.forEach((i) => {
         if (i.path !== path) {
-          i.ele.style.visibility = "hidden";
+          i.ele.style.display = "block";
           window.location.hash = path.toString();
         } else {
-          i.ele.style.visibility = "visible";
+          i.ele.style.display = "none";
         }
       });
     }
+    console.log("%c Change route finish!", "color: red");
   }
 }
 
